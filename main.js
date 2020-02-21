ddaffb8d268e6edb5aff279ba0d0cd9ac83ea4e8
Tzuyuchu <phantombearoftheopera@gmail.com>
2020-02-20 21:29:48 -0800

    Add

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 document.getElementById("title").innerHTML = "Updated :D";
+document.getElementById("button").onclick = function() {
+    document.getElementById("title").innerHTML = "More updoot :P";
+}
