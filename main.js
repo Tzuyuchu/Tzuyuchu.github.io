7777127d295cce45b9a8326bc8b95fb47f4214f5
Tzuyuchu <phantombearoftheopera@gmail.com>
2020-07-19 15:55:10 -0700

    Done

---
+++ main.js
@@ -0,0 +1,22 @@
+function $(x) {return document.querySelector(x);}
+
+var keyword = "cute cats";
+
+    $(document).ready(function(){
+
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {
+            tags: keyword,
+            tagmode: "any",
+            format: "json"
+        },
+        function(data) {
+            var rnd = Math.floor(Math.random() * data.items.length);
+
+            var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+
+            $('body').css('background-image', "url('" + image_src + "')");
+
+        });
+
+    });
